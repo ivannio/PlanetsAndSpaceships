a9b3a0d6e00eba850d64645d09cd242058a8eedf
Ivan Phelps <ivanniothemannio@gmail.com>
2020-03-21 11:01:50 -0500

    Add project files.

---
+++ PlanetsAndSpaceships/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetsAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            var lastPlanets = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.AddRange(lastPlanets);
+
+            planetList.Insert(1, "Earth");
+            planetList.Insert(1, "Venus");
+            planetList.Add("Pluto");
+
+            var rockyPlanets = planetList.GetRange(0, 4);
+
+            planetList.Remove("Pluto");
+
+            var spaceCrafts = new Dictionary<string, List<string>>();
+
+            spaceCrafts.Add("Voyager 1", new List<string> { "Jupiter", "Saturn", "Earth" });
+            spaceCrafts.Add("Voyager 2", new List<string> { "Jupiter", "Saturn", "Earth", "Uranus", "Neptune" });
+            spaceCrafts.Add("New Horizons", new List<string> { "Jupiter", "Pluto" });
+            spaceCrafts.Add("Mariner 10", new List<string> { "Venus", "Mercury" });
+            spaceCrafts.Add("Mars Observer", new List<string> { "Mars" });
+
+            foreach (var planet in planetList)
+            {
+                foreach (var spaceCraft in spaceCrafts)
+                {
+                    if (spaceCraft.Value.Contains(planet))
+                    {
+                        Console.WriteLine($"The {spaceCraft.Key} excursion observed {planet}");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
